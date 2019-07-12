4fa2e525aa32f5b5a4129068f3bb0791be89218a
dafn <dafn@dafns-MacBook-Pro.local>
2019-07-12 21:16:20 +0200

    fix route handle for particles.js

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use("/", (req, res, next) => {
-
   console.log(req.method + " :: " + req.get('host') + req.originalUrl)
-
   next()
 })
+
+app.use('*/assets/', express.static(path.resolve(__dirname, 'dist/assets')))
 
 app.use(express.static(path.resolve(__dirname, 'dist/'), {
   setHeaders: res => {
