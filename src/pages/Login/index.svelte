1e9894fa9e2fa57899bde63e47cc9bdeae56a4a5
dafn <dafn@dafns-MacBook-Pro.local>
2019-07-14 23:18:45 +0200

    style the checkbox

--- src/pages/Login/index.svelte
+++ src/pages/Login/index.svelte
@@ -2,26 +2,77 @@
   import Particles from "../../components/Particles";
   import "./index.css";
 
+  const src = require("../../assets/fermin-inverted.png"),
+    title = "Log in to Fermin",
+    mailPlaceholder = "Epost",
+    passwordPlaceholder = "Passord",
+    LogInButtonText = "Log In",
+    action = location.href + "/login",
+    id = "particles";
+
+  let mail = "",
+    password = "",
+    checked = true;
+
+  $: disabled = !(
+    mail.includes("@") &&
+    password.length >= 8 &&
+    password.length <= 18
+  );
+
   new Particles({
     target: document.querySelector("main"),
     props: {
       id: "particles"
     }
   });
-
-  const src = require("../../assets/fermin-inverted.png"),
-    title = "Log in to Fermin",
-    mailPlaceholder = "Epost",
-    passwordPlaceholder = "Passord",
-    LogInButtonText = "Log In",
-    action = location.href + '/login',
-    id = "particles"
-
-  let mail = "",
-    password = "";
-
-  $: disabled = !(mail.includes("@") && password.length >= 8 && password.length <= 18)
 </script>
+
+<style>
+  #checkbox_container {
+    position: relative;
+    padding-left: 23px;
+    user-select: none;
+    margin: 5px auto 0 25px;
+  }
+  #checkbox_container input {
+    position: absolute;
+    opacity: 0;
+    height: 0;
+    width: 0;
+  }
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 14px;
+    width: 14px;
+    transition: background 0.1s ease;
+    border: solid 1px #bfbfbf;
+    border-radius: 3px;
+  }
+  #checkbox_container input:checked ~ .checkmark {
+    border-color: #2185d0;
+    background: #2185d0;
+  }
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+  #checkbox_container input:checked ~ .checkmark:after {
+    display: block;
+  }
+  #checkbox_container .checkmark:after {
+    left: 4px;
+    top: 2px;
+    width: 3px;
+    height: 6px;
+    border: solid white;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+  }
+</style>
 
 <section>
   <form {action} method="POST">
@@ -45,14 +96,13 @@
         bind:value={password} />
     </div>
 
-    <div id="checkbox_container">
-      <input type="checkbox" name="remember" value="" />
-      <label for="checkbox">Remember Me</label>
-    </div>
+    <label id="checkbox_container">
+      Remember Me
+      <input {checked} type="checkbox" name="remember" value="" />
+      <span class="checkmark" />
+    </label>
 
-    <button class:disabled {disabled}>
-      {LogInButtonText}
-    </button>
+    <button class:disabled {disabled}>{LogInButtonText}</button>
 
     <input id="prompt" name="prompt" value="login" />
   </form>
