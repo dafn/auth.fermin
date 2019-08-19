6a2a4c7ba22ae979bbd880d1a9dd54a32ade7594
dafn <dafn@dafns-MacBook-Pro.local>
2019-08-19 17:09:52 +0200

    add color box for later maybe

---
+++ src/components/Fermin/index.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { onMount } from "svelte";
+
+  let canvas,
+    running = false;
+
+  const r = Math.random();
+
+  onMount(() => {
+    const ctx = canvas.getContext("2d");
+    let frame, loop;
+
+    (loop = () => {
+      frame = requestAnimationFrame(loop);
+
+      const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+      for (let p = 0; p < image.data.length; p += 4) {
+        const i = p / 4;
+        const x = i % canvas.width;
+        const y = (i / canvas.height) >>> 0;
+
+        const t = window.performance.now();
+
+        const r = 64 + (128 * x) / canvas.width + 64 * Math.sin(t / 2000),
+          g = 64 + (128 * y) / canvas.height + 64 * Math.cos(t / 2000),
+          b = 128;
+
+        image.data[p + 0] = r;
+        image.data[p + 1] = g;
+        image.data[p + 2] = b;
+        image.data[p + 3] = 255;
+      }
+      ctx.putImageData(image, 0, 0);
+    })();
+
+    return () => {
+      cancelAnimationFrame(frame);
+    };
+  });
+</script>
+
+<style>
+  canvas {
+    width: 100px;
+    height: 100px;
+    background-color: #666;
+    -webkit-mask: url(../../assets/blackbox.svg) 50% 50% no-repeat;
+    mask: url(../../assets/blackbox.svg) 50% 50% no-repeat;
+  }
+</style>
+
+<canvas bind:this={canvas} width={32} height={32} />
